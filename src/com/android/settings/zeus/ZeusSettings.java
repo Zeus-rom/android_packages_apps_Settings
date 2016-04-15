0bfa90b3a38481018970a73c2d6daf511c5421cd
Serafin A. Albiero Jr <serajr@gmail.com>
2016-04-15 12:37:47 +0530

    Blurred SystemUI

--- src/com/android/settings/zeus/ZeusSettings.java
+++ src/com/android/settings/zeus/ZeusSettings.java
@@ -1,5 +1,32 @@
 package com.android.settings.zeus;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.TwoStatePreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.view.View;
+
+import com.android.settings.zeus.SeekBarPreference;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.content.res.Resources;
 import android.preference.Preference;
@@ -9,12 +36,21 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 import android.provider.Settings;
 
+import cyanogenmod.providers.CMSettings;
 import com.android.internal.logging.MetricsLogger;
 
 public class ZeusSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "ZeusSettings";
+
+    private TwoStatePreference mExpand;
+    private TwoStatePreference mNotiTrans;
+    private TwoStatePreference mHeadSett;
+    private TwoStatePreference mQuickSett;
+    private TwoStatePreference mEditButton;
+    private SeekBarPreference mScale;
+    private SeekBarPreference mRadius;
 
     @Override
     protected int getMetricsCategory() {
@@ -25,6 +61,52 @@ public class ZeusSettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.zeus_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mExpand = (TwoStatePreference) findPreference("hook_system_ui_blurred_status_bar_expanded_enabled_pref");
+        boolean mExpandint = (Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, 1) == 1);
+        mExpand.setChecked(mExpandint);
+        mExpand.setOnPreferenceChangeListener(this);
+
+        mScale = (SeekBarPreference) findPreference("statusbar_blur_scale");
+        mScale.setValue(CMSettings.System.getInt(resolver, CMSettings.System.STATUSBAR_BLUR_SCALE, 10));
+        mScale.setOnPreferenceChangeListener(this);
+
+        mRadius = (SeekBarPreference) findPreference("statusbar_blur_radius");
+        mRadius.setValue(CMSettings.System.getInt(resolver, CMSettings.System.STATUSBAR_BLUR_RADIUS, 5));
+        mRadius.setOnPreferenceChangeListener(this);
+
+        mNotiTrans = (TwoStatePreference) findPreference("hook_system_ui_translucent_notifications_pref");
+        boolean mNotiTransint = (Settings.System.getInt(resolver,
+                Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, 1) == 1);
+        mNotiTrans.setChecked(mNotiTransint);
+        mNotiTrans.setOnPreferenceChangeListener(this);
+
+        mHeadSett = (TwoStatePreference) findPreference("hook_system_ui_translucent_header_pref");
+        boolean mHeadSettint = (Settings.System.getInt(resolver,
+                Settings.System.TRANSLUCENT_HEADER_PREFERENCE_KEY, 1) == 1);
+        mHeadSett.setChecked(mHeadSettint);
+        mHeadSett.setOnPreferenceChangeListener(this);
+
+        mQuickSett = (TwoStatePreference) findPreference("hook_system_ui_translucent_quick_settings_pref");
+        boolean mQuickSettint = (Settings.System.getInt(resolver,
+                Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, 1) == 1);
+        mQuickSett.setChecked(mQuickSettint);
+        mQuickSett.setOnPreferenceChangeListener(this);
+
+        mEditButton = (TwoStatePreference) findPreference("hook_statusbar_editbutton_pref");
+        boolean mEditButtonint = (Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_EDITBUTTON_PREFERENCE_KEY, 1) == 1);
+        mEditButton.setChecked(mEditButtonint);
+        mEditButton.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        // todo add a constant in MetricsLogger.java
+        return MetricsLogger.MAIN_SETTINGS;
     }
 
     @Override
@@ -33,6 +115,67 @@ public class ZeusSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        Intent i = new Intent("serajr.blurred.system.ui.lp.UPDATE_PREFERENCES");
+        if (preference == mExpand) {
+            Settings.System.putInt(
+                    resolver, Settings.System.STATUS_BAR_EXPANDED_ENABLED_PREFERENCE_KEY, (((Boolean) newValue) ? 1 : 0));
+            getContext().sendBroadcast(i);
+            return true;
+        } else if (preference == mScale) {
+            CMSettings.System.putInt(
+                resolver, CMSettings.System.STATUSBAR_BLUR_SCALE, (Integer) newValue);
+            getContext().sendBroadcast(i);
+            return true;
+        } else if (preference == mRadius) {
+            int value = Integer.parseInt((String) newValue);
+            CMSettings.System.putInt(
+                resolver, CMSettings.System.STATUSBAR_BLUR_RADIUS, (Integer) newValue);
+            getContext().sendBroadcast(i);
+            return true;
+        } else if (preference == mNotiTrans) {
+            Settings.System.putInt(
+                    resolver, Settings.System.TRANSLUCENT_NOTIFICATIONS_PREFERENCE_KEY, (((Boolean) newValue) ? 1 : 0));
+            getContext().sendBroadcast(i);
+            return true;
+        } else if (preference == mHeadSett) {
+            Settings.System.putInt(
+                    resolver, Settings.System.TRANSLUCENT_HEADER_PREFERENCE_KEY, (((Boolean) newValue) ? 1 : 0));
+            getContext().sendBroadcast(i);
+            return true;
+        } else if (preference == mQuickSett) {
+            Settings.System.putInt(
+                    resolver, Settings.System.TRANSLUCENT_QUICK_SETTINGS_PREFERENCE_KEY, (((Boolean) newValue) ? 1 : 0));
+            getContext().sendBroadcast(i);
+            return true;
+        } else if (preference == mEditButton) {
+            Settings.System.putInt(
+                    resolver, Settings.System.STATUSBAR_EDITBUTTON_PREFERENCE_KEY, (((Boolean) newValue) ? 1 : 0));
+            getContext().sendBroadcast(i);
+            return true;
+        }
         return false;
     }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.zeus_settings;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+                    return result;
+                }
+            };
 }
