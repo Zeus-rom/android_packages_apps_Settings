AOKP/packages_apps_Settings

[SQUASHED] Settings : Add DU Fling,Pulse,SmartBar
Action Picker Dialog :

http://gerrit.dirtyunicorns.com/23916

Author:Randall Rushing <randall.rushing@gmail.com>

Expose Navbar for External Fragment Launching

http://gerrit.dirtyunicorns.com/23914

Author:Randall Rushing <randall.rushing@gmail.com>

Settings: Support Bringup

http://gerrit.dirtyunicorns.com/24717

Author:Randall Rushing <randall.rushing@gmail.com>

Revert old options :-
SLim Dim
CM navbar
Slim Navbar Dimensions

AICPfy:
- BIG THANKS TO RR team
- Bring to AICP

Change-Id: Ic981d9e730931095652d8b6081462d2f9bf46a19

src/com/android/settings/aicp/IconPickHelper.java
@@ -1,0 +1,80 @@
+/**
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ * Author: Randall Rushing <randall.rushing@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Wrapper class for delivering icon picker results to activities or fragments
+ */
+
+package com.android.settings.aicp;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+
+public class IconPickHelper {
+    public static final int REQUEST_PICK_ICON_PACK = 100;
+    public static final int REQUEST_PICK_ICON_GALLERY = 101;
+    public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.android.settings.aicp.IconPickerActivity";
+    private static final int REQUEST_CODE = 420;
+    private Activity mParent;
+    private OnPickListener mListener;
+    private int lastFragmentId;
+    private int mIconPickType;
+
+    public interface OnPickListener {
+        void iconPicked(String iconType, String iconPackage, String iconName);
+    }
+
+    public IconPickHelper(Activity parent, OnPickListener listener) {
+        mParent = parent;
+        mListener = listener;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE:
+                    String iconType = data.getStringExtra("icon_data_type");
+                    String iconPackage = data.getStringExtra("icon_data_package");
+                    String iconName = data.getStringExtra("icon_data_name");
+                    mListener.iconPicked(iconType, iconPackage, iconName);
+                    break;
+            }
+        }
+    }
+
+    public void pickIcon(int fragmentId, int requestType) {
+        lastFragmentId = fragmentId;
+        mIconPickType = requestType;
+        if (requestType == REQUEST_PICK_ICON_PACK) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.setClassName(INTENT_ACTION_EDIT_CLASS, INTENT_ACTION_ICON_PICKER_COMPONENT);
+            startFragmentOrActivity(intent, REQUEST_CODE);
+        }
+    }
+
+    private void startFragmentOrActivity(Intent pickIntent, int requestCode) {
+        if (lastFragmentId == 0) {
+            mParent.startActivityForResult(pickIntent, requestCode);
+        } else {
+            Fragment cFrag = mParent.getFragmentManager().findFragmentById(lastFragmentId);
+            if (cFrag != null) {
+                mParent.startActivityFromFragment(cFrag, pickIntent, requestCode);
+            }
+        }
+    }
+}

src/com/android/settings/aicp/SmartbarSettings.java
@@ -1,0 +1,133 @@
+/*
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aicp;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.utils.du.ActionConstants;
+import com.android.internal.utils.du.Config;
+import com.android.internal.utils.du.Config.ButtonConfig;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class SmartbarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    private ListPreference mSmartBarContext;
+    private ListPreference mImeActions;
+    private ListPreference mButtonAnim;
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.smartbar_settings);
+
+        mContext = getContext();
+
+        int contextVal = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                "smartbar_context_menu_mode", 0, UserHandle.USER_CURRENT);
+        mSmartBarContext = (ListPreference) findPreference("smartbar_context_menu_position");
+        mSmartBarContext.setValue(String.valueOf(contextVal));
+        mSmartBarContext.setOnPreferenceChangeListener(this);
+
+        int imeVal = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                "smartbar_ime_hint_mode", 1, UserHandle.USER_CURRENT);
+        mImeActions = (ListPreference) findPreference("smartbar_ime_action");
+        mImeActions.setValue(String.valueOf(imeVal));
+        mImeActions.setOnPreferenceChangeListener(this);
+
+        int buttonAnimVal = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                "smartbar_button_animation_style", 0, UserHandle.USER_CURRENT);
+        mButtonAnim = (ListPreference) findPreference("smartbar_button_animation");
+        mButtonAnim.setValue(String.valueOf(buttonAnimVal));
+        mButtonAnim.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == findPreference("smartbar_editor_mode")) {
+            getActivity().sendBroadcastAsUser(new Intent("intent_navbar_edit"), UserHandle.CURRENT);
+            return true;
+        } else if (preference == findPreference("smartbar_factory_reset")) {
+            new AlertDialog.Builder(getActivity())
+                    .setMessage(getString(R.string.smartbar_factory_reset_confirm))
+                    .setPositiveButton(android.R.string.ok, new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ArrayList<ButtonConfig> buttonConfigs = Config.getDefaultConfig(
+                                    mContext,
+                                    ActionConstants.getDefaults(ActionConstants.SMARTBAR));
+                            Config.setConfig(mContext,
+                                    ActionConstants.getDefaults(ActionConstants.SMARTBAR),
+                                    buttonConfigs);
+                            Intent intent = new Intent("intent_navbar_edit");
+                            intent.putExtra("extra_navbar_edit_reset_layout", "resetMePlox");
+                            getActivity().sendBroadcastAsUser(intent, UserHandle.CURRENT);
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mSmartBarContext)) {
+            int position = Integer.parseInt(((String) newValue).toString());
+            Settings.Secure.putInt(getContentResolver(), "smartbar_context_menu_mode",
+                    position);
+            return true;
+        } else if (preference.equals(mButtonAnim)) {
+            int val = Integer.parseInt(((String) newValue).toString());
+            Settings.Secure.putInt(getContentResolver(), "smartbar_button_animation_style",
+                    val);
+            return true;
+        } else if (preference.equals(mImeActions)) {
+            int val = Integer.parseInt(((String) newValue).toString());
+            Settings.Secure.putInt(getContentResolver(), "smartbar_ime_hint_mode",
+                    val);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+}

Zeus-rom/android_packages_apps_Settings

Conflicts:
	Android.mk
	AndroidManifest.xml
	res/values/aicp_arrays.xml
	res/values/aicp_strings.xml
	res/xml/button_settings.xml
	res/xml/dashboard_categories.xml
	res/xml/navbar_dimensions.xml
	src/com/android/settings/ButtonSettings.java
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/zeus/IconPickHelper.java
@@ -1,0 +1,80 @@
+/**
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ * Author: Randall Rushing <randall.rushing@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Wrapper class for delivering icon picker results to activities or fragments
+ */
+
+package com.android.settings.zeus;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+
+public class IconPickHelper {
+    public static final int REQUEST_PICK_ICON_PACK = 100;
+    public static final int REQUEST_PICK_ICON_GALLERY = 101;
+    public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.android.settings.zeus.IconPickerActivity";
+    private static final int REQUEST_CODE = 420;
+    private Activity mParent;
+    private OnPickListener mListener;
+    private int lastFragmentId;
+    private int mIconPickType;
+
+    public interface OnPickListener {
+        void iconPicked(String iconType, String iconPackage, String iconName);
+    }
+
+    public IconPickHelper(Activity parent, OnPickListener listener) {
+        mParent = parent;
+        mListener = listener;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE:
+                    String iconType = data.getStringExtra("icon_data_type");
+                    String iconPackage = data.getStringExtra("icon_data_package");
+                    String iconName = data.getStringExtra("icon_data_name");
+                    mListener.iconPicked(iconType, iconPackage, iconName);
+                    break;
+            }
+        }
+    }
+
+    public void pickIcon(int fragmentId, int requestType) {
+        lastFragmentId = fragmentId;
+        mIconPickType = requestType;
+        if (requestType == REQUEST_PICK_ICON_PACK) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.setClassName(INTENT_ACTION_EDIT_CLASS, INTENT_ACTION_ICON_PICKER_COMPONENT);
+            startFragmentOrActivity(intent, REQUEST_CODE);
+        }
+    }
+
+    private void startFragmentOrActivity(Intent pickIntent, int requestCode) {
+        if (lastFragmentId == 0) {
+            mParent.startActivityForResult(pickIntent, requestCode);
+        } else {
+            Fragment cFrag = mParent.getFragmentManager().findFragmentById(lastFragmentId);
+            if (cFrag != null) {
+                mParent.startActivityFromFragment(cFrag, pickIntent, requestCode);
+            }
+        }
+    }
+}

src/com/android/settings/zeus/SmartbarSettings.java
@@ -1,0 +1,133 @@
+/*
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.zeus;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.utils.du.ActionConstants;
+import com.android.internal.utils.du.Config;
+import com.android.internal.utils.du.Config.ButtonConfig;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class SmartbarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    private ListPreference mSmartBarContext;
+    private ListPreference mImeActions;
+    private ListPreference mButtonAnim;
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.smartbar_settings);
+
+        mContext = getContext();
+
+        int contextVal = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                "smartbar_context_menu_mode", 0, UserHandle.USER_CURRENT);
+        mSmartBarContext = (ListPreference) findPreference("smartbar_context_menu_position");
+        mSmartBarContext.setValue(String.valueOf(contextVal));
+        mSmartBarContext.setOnPreferenceChangeListener(this);
+
+        int imeVal = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                "smartbar_ime_hint_mode", 1, UserHandle.USER_CURRENT);
+        mImeActions = (ListPreference) findPreference("smartbar_ime_action");
+        mImeActions.setValue(String.valueOf(imeVal));
+        mImeActions.setOnPreferenceChangeListener(this);
+
+        int buttonAnimVal = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                "smartbar_button_animation_style", 0, UserHandle.USER_CURRENT);
+        mButtonAnim = (ListPreference) findPreference("smartbar_button_animation");
+        mButtonAnim.setValue(String.valueOf(buttonAnimVal));
+        mButtonAnim.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == findPreference("smartbar_editor_mode")) {
+            getActivity().sendBroadcastAsUser(new Intent("intent_navbar_edit"), UserHandle.CURRENT);
+            return true;
+        } else if (preference == findPreference("smartbar_factory_reset")) {
+            new AlertDialog.Builder(getActivity())
+                    .setMessage(getString(R.string.smartbar_factory_reset_confirm))
+                    .setPositiveButton(android.R.string.ok, new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ArrayList<ButtonConfig> buttonConfigs = Config.getDefaultConfig(
+                                    mContext,
+                                    ActionConstants.getDefaults(ActionConstants.SMARTBAR));
+                            Config.setConfig(mContext,
+                                    ActionConstants.getDefaults(ActionConstants.SMARTBAR),
+                                    buttonConfigs);
+                            Intent intent = new Intent("intent_navbar_edit");
+                            intent.putExtra("extra_navbar_edit_reset_layout", "resetMePlox");
+                            getActivity().sendBroadcastAsUser(intent, UserHandle.CURRENT);
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mSmartBarContext)) {
+            int position = Integer.parseInt(((String) newValue).toString());
+            Settings.Secure.putInt(getContentResolver(), "smartbar_context_menu_mode",
+                    position);
+            return true;
+        } else if (preference.equals(mButtonAnim)) {
+            int val = Integer.parseInt(((String) newValue).toString());
+            Settings.Secure.putInt(getContentResolver(), "smartbar_button_animation_style",
+                    val);
+            return true;
+        } else if (preference.equals(mImeActions)) {
+            int val = Integer.parseInt(((String) newValue).toString());
+            Settings.Secure.putInt(getContentResolver(), "smartbar_ime_hint_mode",
+                    val);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+}